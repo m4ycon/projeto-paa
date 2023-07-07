5dd17b01f7c38b75fa11c64340332a583c6a9b4d
Maycon Fabio <m.vinnicyus@gmail.com>
2023-07-07 18:01:18 -0300

    Create a class for gpt scrapper

--- chatgpt-scrapper/main.py
+++ chatgpt-scrapper/main.py
@@ -6,22 +6,29 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-model_name = "gpt-3.5-turbo"
-messages = []
-messages.append({
-  "role": "system", 
-  "content": "You are a scrapper robot that generates formatted questions and answers from pokemon universe"
-})
+class GptScrapper:
+  def __init__(self, model_name = "gpt-3.5-turbo"):
+    system_def = open('./chatgpt-scrapper/data/system.txt', mode="r", encoding="utf-8").read()
+    self.model_name = model_name
+    self.messages = []
+    self.messages.append({ "role": "system", "content": system_def })
+    self.total_tokens = 0
 
-total_tokens = 0
+  def chat(self, message):
+    self.messages.append({"role": "user", "content": message})
+
+    response = openai.ChatCompletion.create(model=self.model_name, messages=self.messages)
+    reply = response["choices"][0]["message"]["content"]
+    self.total_tokens += response["usage"]["total_tokens"]
+    
+    self.messages.append({"role": "assistant", "content": reply})
+
+    return reply
+
+
+gptScrapper = GptScrapper()
 
 while True:
   message = input('\nuser > ')
-  messages.append({"role": "user", "content": message})
-
-  response = openai.ChatCompletion.create(model=model_name, messages=messages)
-  reply = response["choices"][0]["message"]["content"]
-  total_tokens += response["usage"]["total_tokens"]
-  
-  messages.append({"role": "assistant", "content": reply})
-  print(f'\n{model_name} ({total_tokens}) > {reply}')
+  reply = gptScrapper.chat(message)
+  print(f'\n{gptScrapper.model_name} ({gptScrapper.total_tokens}) > {reply}')
